6f9b94004f7fbc522ee6a5fc7ac0bf4b98db7f6a
Chenwei Yin <yin.chenw@husky.neu.edu>
2017-06-18 14:41:50 -0400

    fix validation

--- assignment/model/user/user.schema.server.js
+++ assignment/model/user/user.schema.server.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},//String,
-    password: {type: String, required: true},
+    password: String,
     firstName: String,
     lastName: String,
     email: String,
